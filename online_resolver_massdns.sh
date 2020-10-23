4957630d2cac61bb4c6ab36f381c8f5f439d5f2b
Pasi Saarinen <phasip@gmail.com>
2020-10-23 09:17:25 +0200

    Update online_resolver_massdns.sh

--- online_resolver_massdns.sh
+++ online_resolver_massdns.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
-wget -q -O /resolvers.txt https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt
+wget -q -O /resolvers.txt https://raw.githubusercontent.com/phasip/resolvers/master/resolvers.txt
 /massdns/bin/massdns -r /resolvers.txt "$@"
