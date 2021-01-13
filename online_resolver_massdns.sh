f90cc9088d272731b55b7ca54fc28868ffcf8899
Pasi Saarinen <phasip@gmail.com>
2021-01-13 09:23:17 +0100

    Randomize resolvers before use

--- online_resolver_massdns.sh
+++ online_resolver_massdns.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 wget -q -O /resolvers.txt https://raw.githubusercontent.com/phasip/resolvers/master/resolvers-stable-grade11.txt
+sort -rU -o /resolvers.txt /resolvers.txt
 /massdns/bin/massdns -r /resolvers.txt "$@"
